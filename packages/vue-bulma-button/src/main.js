3b06e11d70e8dddec15676e7cb2db9d0f7f29acf
dameng <pingf0@gmail.com>
2019-04-19 17:56:00 +0800

    test

--- packages/vue-bulma-button/src/main.js
+++
@@ -1,6 +0,0 @@
-import './style.scss'
-
-export { default as Button } from './Button'
-export { default as ButtonAddons } from './ButtonAddons'
-export { default as ButtonGroup } from './ButtonGroup'
-export { default as ButtonsList } from './ButtonsList'
