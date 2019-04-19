21ec1efe79c1ec848482f503b3e10f60a5e582b2
dameng <pingf0@gmail.com>
2019-04-19 17:54:54 +0800

    add custom prefix

---
+++ packages/@mergeforward/vue-bulma/src/main.js
@@ -0,0 +1,77 @@
+// // Columns
+// export { Column, Columns } from 'vue-bulma-columns'
+
+// // Layouts
+// export { default as Container } from 'vue-bulma-container'
+
+// export { Level, LevelItem } from 'vue-bulma-level'
+
+// export { default as MediaObject } from 'vue-bulma-media-object'
+
+// export { default as Section } from 'vue-bulma-section'
+
+// export { default as Hero } from 'vue-bulma-hero'
+
+// export { default as Footer } from 'vue-bulma-footer'
+
+// export { default as Tile } from 'vue-bulma-tile'
+
+// // Form
+// export { Form, FormItem } from 'vue-bulma-form'
+
+// export { default as Input } from 'vue-bulma-input'
+
+// export { default as Textarea } from 'vue-bulma-textarea'
+
+// export { default as Select } from 'vue-bulma-select'
+
+// export { Checkbox, CheckboxGroup } from 'vue-bulma-checkbox'
+
+// export { Radio, RadioGroup } from 'vue-bulma-radio'
+
+// export { default as Uploader } from 'vue-bulma-uploader'
+
+// // Elements
+// export { default as Box } from 'vue-bulma-box'
+
+export {
+  Button,
+  ButtonAddons,
+  ButtonGroup,
+  ButtonsList
+} from '@mergeforward/vue-bulma-button'
+
+// export { default as Content } from 'vue-bulma-content'
+
+// export { default as Delete } from 'vue-bulma-delete'
+
+// export { default as Icon } from 'vue-bulma-icon'
+
+// export { default as Image } from 'vue-bulma-image'
+
+// export { default as Progress } from 'vue-bulma-progress'
+
+// export { default as Notification } from 'vue-bulma-notification'
+
+// export { Tag, Tags } from 'vue-bulma-tag'
+
+// export { Title, Subtitle } from 'vue-bulma-title'
+
+// // Components
+// export { Breadcrumb, BreadcrumbItem } from 'vue-bulma-breadcrumb'
+
+// export { Card, CardFooterItem } from 'vue-bulma-card'
+
+// export { Dropdown, DropdownItem, DropdownMenu } from 'vue-bulma-dropdown'
+
+// export { Menu, MenuList, MenuItem } from 'vue-bulma-menu'
+
+// export { default as Message } from 'vue-bulma-message'
+
+// export { default as Modal } from 'vue-bulma-modal'
+
+// export { Navbar, NavbarDropdown, NavbarItem } from 'vue-bulma-navbar'
+
+// export { default as Pagination } from 'vue-bulma-pagination'
+
+// export { Tabs, TabItem } from 'vue-bulma-tabs'
